ceab4bdd8fd61ab197d3e494e4d9d9ab1b5f5fb1
Patrick Bell <zclpatrick@gmail.com>
2022-11-29 11:36:54 +1000

    fix shaders but its still cooked

--- src/audio.rs
+++ src/audio.rs
@@ -8,57 +8,36 @@ pub fn vol_to_db(vol: f32) -> f32 {
     20.0f32 * vol.log10()
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 pub struct SoundDesc {
-    pub frames: [TimbreFrame; 2],
-    pub t: f32,
-    pub delete: bool,
+    pub f: f32,
+    pub n: f32,
+    pub troll: f32,
+    pub ea: f32,
+    pub ed: f32,
+    pub es: f32,
+    pub er: f32,
+    pub detune: f32,
+    pub voices: f32,
+    pub amp: f32,
+    pub cut: f32,
+    pub cur: f32,
+    pub cdt: f32,
+    pub cdr: f32,
+    pub aout: f32,
+    pub release: bool,
 }
 
-// what if it only is one lerp. adsr release can upload the new one anyway
-
-impl SoundDesc {
-    fn curr_tf(&self, t: f32) -> TimbreFrame {
-        if t > self.t {
-            self.frames[1]
-        } else {
-            let t = t / self.t;
-            TimbreFrame {
-                f: lerp(self.frames[0].f, self.frames[1].f, t),
-                sub: lerp(self.frames[0].sub, self.frames[1].sub, t),
-                root: lerp(self.frames[0].root, self.frames[1].root, t),
-                mid: lerp(self.frames[0].mid, self.frames[1].mid, t),
-                high: lerp(self.frames[0].high, self.frames[1].high, t),
-                amp: lerp(self.frames[0].amp, self.frames[1].amp, t),
-                detune: lerp(self.frames[0].detune, self.frames[1].detune, t),
-                voices: lerp(self.frames[0].voices, self.frames[1].voices, t),
-            }
-        }
-    }
-}
-
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 pub struct SoundCommand {
     pub sd: SoundDesc,
     pub id: u32,
 }
 
-#[derive(Clone, Copy)]
-pub struct TimbreFrame {
-    pub f: f32,
-    pub sub: f32,
-    pub root: f32,
-    pub mid: f32,
-    pub high: f32,
-    pub amp: f32,
-    pub detune: f32,
-    pub voices: f32,
-}
-
 pub struct Channel {
     pub sd: SoundDesc,
     pub age: f32,
-    pub phases: Vec<[f32; 20]>,
+    pub phases: Vec<f32>,
     pub id: u32,
 }
 
@@ -66,53 +45,62 @@ impl Channel {
     pub fn tick(&mut self) -> f32 {
         self.age += 1.0 / 44100.0;
 
-        // return (self.sd.frames[0].f * self.age * 2.0 * PI).sin();
+        let sd = self.sd;
 
-        let tf = self.sd.curr_tf(self.age);
+        let voices_len = sd.voices.floor() as usize;
+        let n_len = sd.n.floor() as usize;
+        self.phases.resize(voices_len*n_len, 0.0);  // or resize with random phases, is better
 
-        let voices_len = tf.voices.floor() as usize;
-        self.phases.resize(voices_len, [0.0; 20]);  // or resize with random phases, is better
-
+        // pre compression
         let mut acc = 0.0;
 
-        let a_vol = db_to_vol(tf.amp);
+        let a_vol = db_to_vol(sd.amp);
+
+        let a_env = 1.0;
+
+        let a_voices = 1.0 / voices_len as f32;
 
         for detune_voice_num in 0..voices_len {
-            for n in 0..20 {
-                let f = if n == 0 {
-                    tf.f / 2.0
-                } else {
-                    n as f32 * tf.f
-                };
+            for n in 0..n_len {
+                let a_roll = 1.0 / ((n+1) as f32).powf(sd.troll);
 
-                let local_a = if n == 0 {
-                    tf.sub
-                } else if n == 1 {
-                    tf.root
-                } else if n < 4 {
-                    tf.mid
-                } else {
-                    tf.high
-                };
-
-                let a_roll = if n <= 1 {
-                    1.0
-                } else {
-                    1.0 / n as f32
-                };
-
-                let detune_interval = 2.0f32.powf(tf.detune / 1200.0);
+                let detune_interval = 2.0f32.powf(sd.detune / 1200.0);
+                let f = sd.f * (n + 1) as f32;
                 let f = f * detune_interval.powf(detune_voice_num as f32);
 
-                self.phases[detune_voice_num as usize][n as usize] = (self.phases[detune_voice_num as usize][n as usize] + f / 44100.0).fract();
-                acc += a_roll * a_vol * local_a * (2.0 * PI * self.phases[detune_voice_num as usize][n as usize]).sin();
+                let idx = detune_voice_num * n_len + n;
+                self.phases[idx] = (self.phases[idx] + f / 44100.0).fract();
+                acc += a_voices * a_env * a_roll * a_vol * (2.0 * PI * self.phases[idx]).sin();
             }
         }
-        acc
+
+
+        // now do compression
+        // change db value or amplitude value?
+        let comp = {
+            let cut_vol = db_to_vol(sd.cut);
+            let cdt_vol = db_to_vol(sd.cdt);
+            if acc < cut_vol {
+                let gain = lerp(sd.cur, 1.0, (sd.cur - acc)/sd.cur);
+                gain * acc
+            } else if acc > cdt_vol {
+                cdt_vol + (acc - cdt_vol) / sd.cdr
+            } else {
+                acc
+            }
+        };
+        let out = comp * sd.aout;
+        if out > 1.0 {
+            1.0
+        } else if out < -1.0 {
+            -1.0
+        } else {
+            out
+        }
     }
 
     pub fn should_remove(&self) -> bool {
-        self.sd.delete && self.age > self.sd.t
+        self.sd.release && self.age > self.sd.er
     }
 }
 
@@ -124,26 +112,24 @@ pub struct Mixer {
 
 impl Mixer {
     pub fn handle_command(&mut self, sc: SoundCommand) {
-        println!("handle command id {} delete {}", sc.id, sc.sd.delete);
+        println!("handle command {:?}", sc);
         for i in 0..self.channels.len() {
             // if its already playing we may want to blend
             if self.channels[i].id == sc.id {
 
-                let curr = self.channels[i].sd.curr_tf(self.channels[i].age);
-
                 self.channels[i].sd = sc.sd;
-
-                self.channels[i].sd.frames[0] = curr; // blend
 
                 self.channels[i].age = 0.0;
                 return;
             }
         }
+        let voices_len = sc.sd.voices.floor() as usize;
+        let n_len = sc.sd.n.floor() as usize;
         self.channels.push(Channel {
             sd: sc.sd,
             id: sc.id,
             age: 0.0,
-            phases: Vec::new(), // todo preallocate max phases for detune
+            phases: vec![0.0; voices_len*n_len], // todo preallocate max phases for detune
         });
     }
 
@@ -158,6 +144,7 @@ impl Mixer {
             acc += self.channels[i].tick();
             
             if self.channels[i].should_remove() { 
+                println!("removing {}", i);
                 self.channels.swap_remove(i);
             }
 
@@ -167,3 +154,26 @@ impl Mixer {
         acc
     }
 }
+
+
+
+// pub fn env_amplitude(a: f32, d: f32, s: f32, r: f32, curr_sample: u32, sample_rate: u32, released_sample: Option<u32>) -> f32 {
+//     // +1 for useful recursion
+//     let A = self.a * sample_rate as f32;
+//     let D = self.d * sample_rate as f32;
+//     let S = self.s;
+//     let R = self.r * sample_rate as f32;
+
+//     if let Some(released_on) = released_sample {
+//         let num_released = curr_sample - released_on;
+//         let release_value = self.amplitude(released_on, sample_rate, None);
+//         return lerp(release_value, 0.0, num_released as f32 / R).max(0.0);
+//     }
+//     if curr_sample as f32 <= A {
+//         return lerp(0.0, 1.0, curr_sample as f32 / A);
+//     }
+//     if curr_sample as f32 <= D + A {
+//         return lerp(1.0, S, (curr_sample as f32 - A)/D);
+//     }
+//     S
+// }
--- src/synth_gui.rs
+++ src/synth_gui.rs
@@ -4,55 +4,74 @@ use crate::kmath::*;
 use crate::widgets::*;
 
 pub struct VSliders {
-    sub: FloatSlider,
-    root: FloatSlider,
-    mid: FloatSlider,
-    high: FloatSlider,
-
-    amp: FloatSlider,
-    detune: FloatSlider,
-    voices: FloatSlider,
+        n: FloatSlider,
+        troll: FloatSlider,
+        ea: FloatSlider,
+        ed: FloatSlider,
+        es: FloatSlider,
+        er: FloatSlider,
+        detune: FloatSlider,
+        voices: FloatSlider,
+        amp: FloatSlider,
+        cut: FloatSlider,
+        cur: FloatSlider,
+        cdt: FloatSlider,
+        cdr: FloatSlider,
+        aout: FloatSlider,
 }
 
 impl Default for VSliders {
     fn default() -> Self {
         VSliders {
-            sub: FloatSlider::new(0.0, 0.0, 1.0, "Sub".to_owned()),
-            root: FloatSlider::new(1.0, 0.0, 1.0, "Root".to_owned()),
-            mid: FloatSlider::new(1.0, 0.0, 1.0, "Mid".to_owned()),
-            high: FloatSlider::new(1.0, 0.0, 1.0, "High".to_owned()),
-            amp: FloatSlider::new(-10.0, -30.0, 0.0, "Amp".to_owned()),
-            detune: FloatSlider::new(0.0, 0.0, 99.0, "Detune".to_owned()),
-            voices: FloatSlider::new(1.0, 1.0, 8.0, "Voices".to_owned()),
+            n: FloatSlider::new(1.0, 1.0, 30.0, "NHARM".to_owned()),
+            troll: FloatSlider::new(1.0, 1.0, 4.0, "TROLL".to_owned()),
+
+            ea: FloatSlider::new(0.1, 0.0, 3.0, "ATTACK".to_owned()),
+            ed: FloatSlider::new(0.1, 0.0, 3.0, "DECAY".to_owned()),
+            es: FloatSlider::new(0.5, 0.0, 1.0, "SUSTAIN".to_owned()),
+            er: FloatSlider::new(0.1, 0.0, 3.0, "RELEASE".to_owned()),
+
+            detune: FloatSlider::new(0.0, 0.0, 99.0, "DETUNE".to_owned()),
+            voices: FloatSlider::new(1.0, 1.0, 8.0, "VOICES".to_owned()),
+
+            amp: FloatSlider::new(-20.0, -30.0, 10.0, "AMP-PRE".to_owned()),
+            
+            cut: FloatSlider::new(-100.0, -100.0, 10.0, "C-UP-T".to_owned()),
+            cur: FloatSlider::new(1.0, 1.0, 8.0, "C-UP-R".to_owned()),
+            
+            cdt: FloatSlider::new(0.0, -30.0, 20.0, "C-DOWN-T".to_owned()),
+            cdr: FloatSlider::new(1.0, 1.0, 8.0, "C-DOWN-R".to_owned()),
+
+            aout: FloatSlider::new(-20.0, -30.0, 10.0, "AMP-OUT".to_owned()),
         }
     }
 }
 
 impl VSliders {
-    fn get_tf(&self, f: f32) -> TimbreFrame {
-        TimbreFrame {
+    fn get_sd(&self, f: f32, release: bool) -> SoundDesc {
+        SoundDesc {
             f,
-            sub: self.sub.curr(),
-            root: self.root.curr(),
-            mid: self.mid.curr(),
-            high: self.high.curr(),
-            amp: self.amp.curr(),
+            n: self.n.curr(),
+            troll: self.troll.curr(),
+            ea: self.ea.curr(),
+            ed: self.ed.curr(),
+            es: self.es.curr(),
+            er: self.er.curr(),
             detune: self.detune.curr(),
             voices: self.voices.curr(),
+            amp: self.amp.curr(),
+            cut: self.cut.curr(),
+            cur: self.cur.curr(),
+            cdt: self.cdt.curr(),
+            cdr: self.cdr.curr(),
+            aout: self.aout.curr(),
+            release,
         }
     }
 }
 
-static S_PRESS: usize = 0;
-static S_HELD: usize = 1;
-static S_DONE: usize = 2;
-
 pub struct SynthGUI {
-    sliders: [VSliders; 3],
-    curr: usize,
-    curr_copy: Option<usize>,
-    s_t1: FloatSlider,
-    s_t2: FloatSlider,
+    sliders: VSliders,
 
     history: Vec<(usize, f32, f32)>,
 
@@ -62,11 +81,7 @@ pub struct SynthGUI {
 impl Default for SynthGUI {
     fn default() -> Self {
         SynthGUI {
-            sliders: [VSliders::default(), VSliders::default(), VSliders::default()],
-            curr: 0,
-            curr_copy: None,
-            s_t1: FloatSlider::new(0.5, 0.0, 3.0, "attack".to_owned()),
-            s_t2: FloatSlider::new(0.5, 0.0, 3.0, "release".to_owned()),
+            sliders: VSliders::default(),
             history: Vec::new(),
             t_press: [None; 28],
         }
@@ -113,13 +128,7 @@ fn kc_to_note(kc: VirtualKeyCode) -> Option<usize> {
 
 impl SynthGUI {
     pub fn frame(&mut self, inputs: &FrameInputState, outputs: &mut FrameOutputs) {
-        let colours = [
-            Vec4::new(0.9, 0.2, 0.2, 1.0),
-            Vec4::new(0.2, 0.9, 0.2, 1.0),
-            Vec4::new(0.2, 0.2, 0.9, 1.0),
-        ];
-
-
+        let w = 15;
 
         // key presses
         let pressed_keys = inputs.curr_keys.difference(&inputs.prev_keys);
@@ -131,14 +140,7 @@ impl SynthGUI {
                 outputs.sounds.push(
                     SoundCommand {
                         id: note as u32,
-                        sd: SoundDesc {
-                            frames: [
-                                self.sliders[0].get_tf(f),
-                                self.sliders[1].get_tf(f),
-                            ],
-                            t: self.s_t1.curr(),
-                            delete: false,
-                        }
+                        sd: self.sliders.get_sd(f, false),
                     }
                 )
             }
@@ -156,14 +158,7 @@ impl SynthGUI {
                 outputs.sounds.push(
                     SoundCommand {
                         id: note as u32,
-                        sd: SoundDesc {
-                            frames: [
-                                self.sliders[1].get_tf(f),
-                                self.sliders[2].get_tf(f),
-                            ],
-                            t: self.s_t2.curr(),
-                            delete: true,
-                        }
+                        sd: self.sliders.get_sd(f, true),
                     }
                 )
             }
@@ -175,48 +170,29 @@ impl SynthGUI {
             // top
             let r = r.grid_child(0, 0, 1, 3);
             let r = r.dilate_pc(-0.01);
-            outputs.canvas.put_rect(r, 1.01, colours[self.curr]);
+            outputs.canvas.put_rect(r, 1.01, Vec4::new(0.9, 0.2, 0.2, 1.0));
 
-            {
-                // selection pane
-                let r = r.child(0.0, 0.0, 0.1, 1.0);
-                let r = r.dilate_pc(-0.01);
 
-                for i in 0..3 {
-                    let r = r.grid_child(0, i, 1, 3);
-                    let r = r.dilate_pc(-0.05);
-                    if r.contains(inputs.mouse_pos) && inputs.lmb == KeyStatus::JustPressed {
-                        self.curr = i as usize;
-                    }
-                    outputs.canvas.put_rect(r, 1.04, colours[i as usize]);
-                    
-                }
-            }
-
-            let r = r.child(0.1, 0.0, 0.9, 1.0);
-
-            self.sliders[self.curr].sub.frame(inputs, outputs,    r.grid_child(0, 0, 7, 1));
-            self.sliders[self.curr].root.frame(inputs, outputs,   r.grid_child(1, 0, 7, 1));
-            self.sliders[self.curr].mid.frame(inputs, outputs,    r.grid_child(2, 0, 7, 1));
-            self.sliders[self.curr].high.frame(inputs, outputs,   r.grid_child(3, 0, 7, 1));
-            self.sliders[self.curr].amp.frame(inputs, outputs,    r.grid_child(4, 0, 7, 1));
-            self.sliders[self.curr].detune.frame(inputs, outputs, r.grid_child(5, 0, 7, 1));
-            self.sliders[self.curr].voices.frame(inputs, outputs, r.grid_child(6, 0, 7, 1));
+            self.sliders.n.frame(inputs, outputs,    r.grid_child(0, 0, w, 1));
+            self.sliders.troll.frame(inputs, outputs,   r.grid_child(1, 0, w, 1));
+            self.sliders.ea.frame(inputs, outputs,   r.grid_child(2, 0, w, 1));
+            self.sliders.ed.frame(inputs, outputs,   r.grid_child(3, 0, w, 1));
+            self.sliders.es.frame(inputs, outputs,   r.grid_child(4, 0, w, 1));
+            self.sliders.er.frame(inputs, outputs,   r.grid_child(5, 0, w, 1));
+            self.sliders.detune.frame(inputs, outputs,   r.grid_child(6, 0, w, 1));
+            self.sliders.voices.frame(inputs, outputs,   r.grid_child(7, 0, w, 1));
+            self.sliders.amp.frame(inputs, outputs,   r.grid_child(8, 0, w, 1));
+            self.sliders.cur.frame(inputs, outputs,   r.grid_child(10, 0, w, 1));
+            self.sliders.cut.frame(inputs, outputs,   r.grid_child(11, 0, w, 1));
+            self.sliders.cdr.frame(inputs, outputs,   r.grid_child(12, 0, w, 1));
+            self.sliders.cdt.frame(inputs, outputs,   r.grid_child(13, 0, w, 1));
+            self.sliders.aout.frame(inputs, outputs,   r.grid_child(14, 0, w, 1));
         }
         {
             // mid
             let r = r.grid_child(0, 1, 1, 3);
             let r = r.dilate_pc(-0.01);
             outputs.canvas.put_rect(r, 1.01, Vec4::new(0.0, 0.8, 0.4, 1.0));
-            
-            {
-                let r = r.grid_child(0, 0, 7, 1);
-                self.s_t1.frame(inputs, outputs, r);
-            }
-            {
-                let r = r.grid_child(1, 0, 7, 1);
-                self.s_t2.frame(inputs, outputs, r);
-            }
         }
         {
             // bot
